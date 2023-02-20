282c7ab892b8e2770bd63f2fe37c5fb6a18a99a3
Helder <hmedeiros211@gmail.com>
2023-02-20 14:29:56 +0100

    style(theme): :lipstick: globalStyle and defaultTheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import React, { FunctionComponent } from 'react';
-
+import React, { FunctionComponent } from "react";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "ui/theme/global.style";
+import { defaultTheme } from "ui/theme/theme";
 
 const App: FunctionComponent = () => {
   return (
-    <div className="App"></div>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
---
+++ src/ui/theme/theme.ts
@@ -0,0 +1,62 @@
+import { DefaultTheme } from "styled-components";
+
+enum Variant {
+  Like = "like",
+  Ignore = "ignore",
+  SuperLike = "superLike",
+}
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    colors: {
+      [key: string]: string;
+    };
+    fontFamily: string;
+    variants: {
+      [key in Variant]: {
+        main: string;
+        contrast: string;
+      };
+    };
+  }
+}
+
+const emptyVariant = {
+  main: "",
+  contrast: "",
+};
+
+const defaultTheme: DefaultTheme = {
+  colors: {},
+  fontFamily: "",
+  variants: {
+    [Variant.Like]: emptyVariant,
+    [Variant.SuperLike]: emptyVariant,
+    [Variant.Ignore]: emptyVariant,
+  },
+};
+
+defaultTheme.colors.blue = "#26547C";
+defaultTheme.colors.red = "#E3170A";
+defaultTheme.colors.green = "#138A36";
+defaultTheme.colors.white = "#EAEAEA";
+
+defaultTheme.fontFamily =
+  "ProximaNova, system-ui, -apple-system, Segoe UI, Roboto, Ubuntu, Cantarell, Noto Sans, sans-serif, Helvetica, Arial, Segoe UI Emoji, Segoe UI Symbol, Apple Color Emoji, Twemoji Mozilla, Noto Color Emoji, EmojiOne Color, Android Emoji";
+
+defaultTheme.variants.like = {
+  main: defaultTheme.colors.green,
+  contrast: defaultTheme.colors.white,
+};
+
+defaultTheme.variants.superLike = {
+  main: defaultTheme.colors.blue,
+  contrast: defaultTheme.colors.white,
+};
+
+defaultTheme.variants.ignore = {
+  main: defaultTheme.colors.red,
+  contrast: defaultTheme.colors.white,
+};
+
+export { defaultTheme };
