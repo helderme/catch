d388a06ff3e5cace99621c8ede1e1d8865cef3ef
Helder <hmedeiros211@gmail.com>
2023-02-24 15:59:15 +0100

    style(button): :lipstick: change button

--- src/components/Button/index.style.ts
+++ src/components/Button/index.style.ts
@@ -1,3 +1,4 @@
+import { StyledIcon } from "components/Icon/index.style";
 import styled from "styled-components";
 import { VariantType } from "ui/theme/theme";
 
@@ -5,6 +6,28 @@ interface StyledButtonProps {
   variant: VariantType;
 }
 
-const StyledButton = styled.button<StyledButtonProps>``;
+const StyledButton = styled.button<StyledButtonProps>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 24px;
+  cursor: pointer;
+  background-color: rgba(0, 0, 0, 0.2);
+  border: 1px solid ${(props) => props.theme.variants[props.variant].main};
+  border-radius: 50%;
+  transition: all 0.3s ease-in-out;
+
+  ${StyledIcon} {
+    color: ${(props) => props.theme.variants[props.variant].main};
+    transition: all 0.4s ease-in-out;
+  }
+
+  :hover {
+    background: ${(props) => props.theme.variants[props.variant].main};
+    ${StyledIcon} {
+      color: ${(props) => props.theme.variants[props.variant].contrast};
+    }
+  }
+`;
 
 export { StyledButton };
