dcde44dc52c60f4acf617569bc965bbf064f20ad
Helder <hmedeiros211@gmail.com>
2023-02-24 16:03:20 +0100

    refactor(card): :sparkles: update component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,3 +1,4 @@
+import { Action } from "app-types/action";
 import { Cat } from "app-types/cat";
 import Button from "components/Button";
 import { FunctionComponent, HTMLAttributes } from "react";
@@ -11,21 +12,36 @@ import {
 
 interface CardProps {
   cat: Cat;
+  onAction: (action: Action) => void;
 }
 
 type Props = CardProps & HTMLAttributes<HTMLDivElement>;
 
-const Card: FunctionComponent<Props> = ({ cat: Cat, ...rest }) => {
+const Card: FunctionComponent<Props> = ({ cat: Cat, onAction, ...rest }) => {
   return (
     <StyledCard $catId={Cat.id} {...rest}>
       <Footer>
         <DataContainer>
-          <Name>{Cat.name}</Name>
+          <Name>
+            {Cat.name}, {Cat.age}
+          </Name>
         </DataContainer>
         <ButtonsContainer>
-          <Button icon={{ type: "x" }} variant={"ignore"} />
-          <Button icon={{ type: "star" }} variant={"superLike"} />
-          <Button icon={{ type: "heart" }} variant={"like"} />
+          <Button
+            icon={{ type: "x" }}
+            variant={"ignore"}
+            onClick={() => onAction(Action.Ignore)}
+          />
+          <Button
+            icon={{ type: "star" }}
+            variant={"superLike"}
+            onClick={() => onAction(Action.SuperLike)}
+          />
+          <Button
+            icon={{ type: "heart" }}
+            variant={"like"}
+            onClick={() => onAction(Action.Like)}
+          />
         </ButtonsContainer>
       </Footer>
     </StyledCard>
