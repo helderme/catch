38ff276839c301147194ed6debcd14034c438bd4
Helder <hmedeiros211@gmail.com>
2023-02-26 21:01:32 +0100

    refactor: :truck: change api

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -1,4 +1,4 @@
 const getCatImage = (id: string, width: number) =>
-  `https://cataas.com/cat/says/${id}?size=0&width=${width}`;
+  `https://cataas.com/cat/says/${id[0]}?size=0&width=${width}`;
 
 export { getCatImage };
